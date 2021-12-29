69b6729fe58f281f4c3ecc739defa789089c1ecf
Photon1c <35161340+Photon1c@users.noreply.github.com>
2021-12-29 02:27:17 +0000

    Update buy_s.py
    fine tuned with some adjustments

--- app/buy_s.py
+++ app/buy_s.py
@@ -4,6 +4,7 @@ import datetime as dt
 from config import CONSUMER_KEY, REDIRECT_URI, JSON_PATH
 from td.client import TDClient
 import lesl
+import datetime as dt
 
 #Create a new instance of the client
 TDSession = TDClient(
@@ -16,12 +17,15 @@ getmovers = TDSession.get_movers("$DJI", "up", "value")
 getmovers2 = TDSession.get_movers("$DJI", "down", "value")
 gm = pd.json_normalize(getmovers)
 gm2 = pd.json_normalize(getmovers2)
-gmt = gm.loc[gm['last'] < 80]
+mt = gm.loc[gm['last'] < 150]
+gmt = gmt.append(gm2)
 print("The top daily stock gainers are: ")
 print(gm[['change','last', 'symbol', 'totalVolume']])
 print("The top daily stock losers are: ")
 print(gm2[['change','last', 'symbol', 'totalVolume']])
-gm['symbol'].to_csv(r'[PATH]')
+gm[['change','last', 'symbol', 'totalVolume']].to_csv(r'/home/lescua/projects/fin_suite/CheddarButler_v1/csvs/mover_gainers.csv')
+gm2[['change','last', 'symbol', 'totalVolume']].to_csv(r'/home/lescua/projects/fin_suite/CheddarButler_v1/csvs/mover_losers.csv')
+gmt['symbol'].to_csv(r'/[pathtomoverlistcsvfile]')
 gm3 = pd.DataFrame()
 gm4 = pd.DataFrame()
 print("The cumulative gain for the up stocks is:")
@@ -40,7 +44,8 @@ print(gm4)
 s_alt = pd.read_csv(r'[PATH]')
 #Create Symbols From Dataset
 Symbols = s_alt['symbol'].tolist()
-target_symbols = []
+target_symbols_long = []
+target_symbols_short = []
 # iterate over each symbol
 dfs =pd.DataFrame()
 for i in Symbols:  
@@ -50,16 +55,25 @@ for i in Symbols:
     df2["Date"] = stock_df['datetime']
     df2["Close"] = stock_df['close']
     df2["Volume"] = stock_df['volume']
-    df2["MA10"] = stock_df['close'].rolling(4).mean()
+    df2["MA9"] = stock_df['close'].rolling(9).mean()
     df2["Symbol"] =stock['symbol']
-    bool = df2["MA10"].iloc[-1] < df2["Close"].iloc[-1]  ##create Boolean to filter out target stocks
-    if bool == True:
-        target_list = stock['symbol']
-        target_symbols.append(target_list)
-        #print("The testing dataframes look as follow: ")
-        #print(df2.tail(1))
+    bool_long = df2["MA9"].iloc[-1] < df2["Close"].iloc[-1]  ##create Boolean to filter out stocks to go long on.
+    if bool_long == True:
+        target_list_long = stock['symbol']
+        target_symbols_long.append(target_list_long)
+        print("Stock ", i, "is within buying parameters. Dataframe: " , "\n")
+        print(df2.tail(1))
     else:
         print("Stock", i, "is not within buying parameters")
-print("The target symbols are: ")
-print(target_symbols)
-print("Buy Stocks function complete. Initiating Buy Options function...")
+    bool_short = df2["MA9"].iloc[-1] > df2["Close"].iloc[-1]  ##create second boolean to filter out stocks to short.
+    if bool_short == True:
+        target_list_short = stock['symbol']
+        target_symbols_short.append(target_list_short)
+        print("Stock ", i, "is within selling parameters. Dataframe: " , "\n")
+        print(df2.tail(1))
+    else:
+        print("Stock", i, "is not within selling parameters")   
+print("The aggregate mover list is: ", "\n", gmt)
+print("The target symbols to go long on are: ")
+print(target_symbols_long)
+print("The target symbols to short are: ", "\n", target_symbols_short)
