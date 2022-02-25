4d5d5e48656af4fa50e2ff95ced6d6b4b0765e19
Photon1c <35161340+Photon1c@users.noreply.github.com>
2022-02-25 00:22:39 -0800

    Update listreductor.py

--- csvs/listreductor.py
+++ csvs/listreductor.py
@@ -8,11 +8,7 @@ file5 = pd.read_csv("q5.csv")
 file6 = pd.read_csv("q6.csv")
 file7 = pd.read_csv("q7.csv")
 file8 = pd.read_csv("q8.csv")
-file9 = pd.read_csv("q9.csv")
-file10 = pd.read_csv("q10.csv")
-file11 = pd.read_csv("cleanedversion.csv")
-file12 = pd.read_csv("cleanedversion2.csv")
-frames = [file1, file2, file3, file4, file5, file6, file7, file8, file9, file10, file11, file12]
+frames = [file1, file2, file3, file4, file5, file6, file7, file8]
 
 df = pd.concat(frames, ignore_index=True)
 df2 = pd.DataFrame(df['symbol'], columns=["symbol"])
@@ -21,4 +17,4 @@ df2.reset_index(drop=True, inplace=True)
 
 print(df2)
 
-df2.to_csv("cleanedversion3.csv")
+df2.to_csv("cleanedversionxxx.csv")
